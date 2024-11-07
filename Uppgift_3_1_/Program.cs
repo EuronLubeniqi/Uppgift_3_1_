60bf09130e0d972f39fda59f116252e86e3213db
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-11-07 15:51:28 +0100

    Add project files.

---
+++ Uppgift_3_1_/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Uppgift_3_1_
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 16)
+            {
+                Console.WriteLine("Du får inte delta i tävlingen");
+            }
+            else if (ålder > 19)
+            {
+                Console.WriteLine("Du får inte delta i tävlingen");
+            }
+            else
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+            
+
+           
+            
+            
+            
+            
+           
+            
+        }   
+          
+    }
+}
